0917a9c1b0255a828a6cd27b6b31acdfff17f1de
Kyle Qian <kylecqian@gmail.com>
2017-08-05 21:56:18 -0700

    Added comments + added color

--- Assets/scripts/SerialThreadWorker.cs
+++ Assets/scripts/SerialThreadWorker.cs
@@ -1,31 +1,45 @@
 ﻿using System.IO.Ports;
-using UnityEngine;
+using System.Collections;
 
+/**
+ * This class encapsulates the thread routine that will fetch new COM port readings
+ * independent of Unity's Update cycle.
+ */
 public class SerialThreadWorker {
 
     string portName;
     int baudRate;
-    bool keepRunning;
-    object message;
+    int maxBufferSize;
+    bool keepRunning; // Set to false to stop loop
+    Queue messageBuffer;
     SerialPort stream;
 
-    public SerialThreadWorker(string portName, int baudRate) {
+    public SerialThreadWorker(string portName, int baudRate, int maxBufferSize) {
         this.portName = portName;
         this.baudRate = baudRate;
+        this.maxBufferSize = maxBufferSize;
+        messageBuffer = Queue.Synchronized(new Queue());
     }
 
+    // Thread routine to be invoked
     public void Run() {
         stream = new SerialPort(portName, baudRate);
         stream.Open();
         keepRunning = true;
         while (keepRunning) {
-            message = stream.ReadLine();
+            messageBuffer.Enqueue(stream.ReadLine());
+            if (messageBuffer.Count > maxBufferSize)
+                messageBuffer.Dequeue(); 
         }
         stream.Close();
     }
 
+    // Gets the oldest reading in the current queue
     public object GetReading() {
-        return message;
+        if (messageBuffer.Count == 0)
+            return null;
+
+        return messageBuffer.Dequeue();
     }
 
     public void Stop() {
